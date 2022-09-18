5caaca2caaa0916726f32b2d1b73b3c7a19c490d
jokfang <jonathan0her@gmail.com>
2022-09-18 10:26:09 +0200

    vidage du cjs pour passage en public

--- data/info.cjs
+++ data/info.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-    "token": 'MTAxNjc5NzY3MzE3ODc5MjA3Ng.GTnOz-.WriQ334pUwFn3d7QAMfoH1aaugbRnovoa1ZWbw',
-    "host": "bdd.adkynet.com", 
-    "user": "u11534_VHEi919q3T", 
-    "password": "q3hY5r^1^c8ZXsUl43kS3CaD", 
-    "port": "3306", 
-    "database": "s11534_HouseCup"
+    "token": ,
+    "host": , 
+    "user": , 
+    "password": , 
+    "port": , 
+    "database": 
     };
