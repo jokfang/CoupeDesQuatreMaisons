b5c1547366d9d93952ecd2db2c2ca24daef3aadc
jokfang <jonathan0her@gmail.com>
2022-09-18 17:17:08 +0200

    correction de bétise

--- data/info.cjs
+++ data/info.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-  token:
-    "MTAxNjc5NzY3MzE3ODc5MjA3Ng.GGHQ3N.cisAYOBFNejbxRflBH9GheFlfXVMIg1HmXKEPU",
-  host: "bdd.adkynet.com",
-  user: "u11534_VHEi919q3T",
-  password: "q3hY5r^1^c8ZXsUl43kS3CaD",
-  port: "3306",
-  database: "s11534_HouseCup",
+  token: "",
+  host: "",
+  user: "",
+  password: "",
+  port: "",
+  database: "",
 };
