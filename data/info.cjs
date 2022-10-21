3b431ea6d0c5b61591be5288ae6b4e6f2fc6d9c2
jokfang <112979411+jokfang@users.noreply.github.com>
2022-10-21 15:28:44 +0200

    Update info.cjs

--- data/info.cjs
+++ data/info.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  token: "MTAzMjcxMjk3NDgzNTY2Njk1NA.G49OP9.KXGtbLQWIg_PqET_596ueXlvw0az3Xa7QEd-BM",
-  host: "localhost",
-  user: "root",
+  token: "",
+  host: "",
+  user: "",
   password: "",
-  port: "3308",
-  database: "coupedesquatremaisons",
+  port: "",
+  database: "",
 };
