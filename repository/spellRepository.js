c8d45714b4d3e14b1baaa4f04e9fd3e6c7fc63b3
jokfang <jonathan0her@gmail.com>
2022-10-22 11:06:47 +0200

    création des spell en bdd pour alléger le code du duel

---
+++ repository/spellRepository.js
@@ -0,0 +1,31 @@
+import mysql from "mysql2";
+import * as houses from "../data/info.cjs";
+
+export class SpellRepository {
+  constructor() {}
+  //Ajoute des points à une maison en prenant son id et le montant de point à ajouter
+  async getSpells(channel) {
+    const con = mysql.createConnection({
+      host: houses.default.host,
+      user: houses.default.user,
+      password: houses.default.password,
+      port: houses.default.port,
+      database: houses.default.database,
+    });
+    con.connect(function (err) {
+      if (err) {
+        if (err.message.code === "ETIMEDOUT") {
+          console.log("TimeOut de la BDD");
+        }
+      }
+      //console.log("Connected to MySQLDB");
+    });
+    const query = "select * from Spell where channelId = ? and serverId = ?";
+    const retour = await con
+      .promise()
+      .query(query, [channel.id, channel.guildId]);
+    con.end();
+
+    return retour[0];
+  }
+}
