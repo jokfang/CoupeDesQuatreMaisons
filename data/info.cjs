99c18a898ee2042641aaab7063c8280be9e00db1
jokfang <jonathan0her@gmail.com>
2022-11-12 20:08:51 +0100

    retrait du infocjs

--- data/info.cjs
+++ data/info.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-  token:
-    "MTAxNTkzMTYwODc3MzE2OTE5Mw.G9e7__.DlrVQK6bOkjmaOngUfQYUW1N3egCdcZfHEkU9M",
-  host: "bdd.adkynet.com",
-  user: "u12418_qSrYqqjPfw",
-  password: "@HaxxhebR.IrnZSB=l@9Rwiu",
-  port: "3306",
-  database: "s12418_HouseCup",
+  token: "",
+  host: "",
+  user: "",
+  password: "",
+  port: "",
+  database: "",
 };
