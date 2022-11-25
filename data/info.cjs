07fd56b50a1e50b6cb6d1cd60e1e898c790a18bf
jokfang <jonathan0her@gmail.com>
2022-11-25 18:34:48 +0100

    Simplification

--- data/info.cjs
+++ data/info.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-  token: "",
-  host: "",
-  user: "",
-  password: "",
-  port: "",
-  database: "",
+  token:
+    "MTAxNTkzMTYwODc3MzE2OTE5Mw.GuDAAe.AdtIwvyvuVgdHO0Z7dUMMgWx4rVmg8gziZITdw",
+  host: "bdd.adkynet.com",
+  user: "u12418_qSrYqqjPfw",
+  password: "@HaxxhebR.IrnZSB=l@9Rwiu",
+  port: "3306",
+  database: "s12418_HouseCup",
 };
--- librairy/role.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //ID des Roles
-    administrateur: '935655300730613850',
-    moderateur: '936625195656024104'
-}
