aabd7351c1694cd6fe1b397e4033e6d80c3493ad
jokfang <jonathan0her@gmail.com>
2022-12-03 22:02:54 +0100

    that's work écriture dans un fichier a chaque ajout de points, à ajuster

--- data/info.cjs
+++ data/info.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-  token:
-    "MTAxNTkzMTYwODc3MzE2OTE5Mw.G8hRrT.sLz38Ic6CpJNQpzXxAYoGCDIbmRr0UlAN0usxk",
-  host: "bdd.adkynet.com",
-  user: "u12418_qSrYqqjPfw",
-  password: "@HaxxhebR.IrnZSB=l@9Rwiu",
-  port: "3306",
-  database: "s12418_HouseCup",
-};
+  "token": '',
+  "host": "", 
+  "user": "", 
+  "password": "", 
+  "port": "", 
+  "database": ""
+  };
