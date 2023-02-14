7418ecc4e4dcc599a2aec7b0d2d5815f39415df2
jokfang <jonathan0her@gmail.com>
2023-02-14 17:05:25 +0100

    Revert "add"
    This reverts commit c204e876510f86762c8a4d196d11fb4f69563335.

--- data/info.cjs
+++ data/info.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  "token": 'MTAxNTkzMTYwODc3MzE2OTE5Mw.GP5Di0.0NabU26y_Q2ChYD-pQGPkoG6YgXfsIXoMXp-as',
-  "host": "mysql1.par1.adky.net", 
-  "user": "u12418_Q2we7hlbXu", 
-  "password": "r80@dXYHKQUZvFw@NM^I1.SB", 
-  "port": "3306", 
-  "database": "s12418_HouseCup"
+  "token": '',
+  "host": "", 
+  "user": "", 
+  "password": "", 
+  "port": "", 
+  "database": ""
   };
