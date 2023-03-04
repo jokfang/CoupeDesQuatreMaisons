6a2bde6327cbcc2176cbdc166c5257c3a669d17c
jokfang <jonathan0her@gmail.com>
2023-03-04 18:39:35 +0100

    purge

--- data/info.cjs
+++ data/info.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  "token": 'MTAxNTkzMTYwODc3MzE2OTE5Mw.GLinLg.p2H7cwcWT2B7b2FLe88TxVi2jSRSl-IJHAwatw',
-  "host": "mysql1.par1.adky.net", 
-  "user": "u12418_Q2we7hlbXu", 
-  "password": "r80@dXYHKQUZvFw@NM^I1.SB", 
-  "port": "3306", 
-  "database": "s12418_HouseCup"
+  "token": '',
+  "host": "", 
+  "user": "", 
+  "password": "", 
+  "port": "", 
+  "database": ""
   };
